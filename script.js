57b0ea7e2172f562d60a3c01d50520979084a4f8
Jimmy Dillon Griffith <jimmy.dillon.griffith@gmail.com>
2024-09-23 14:14:49 -0400

    created function getComputerChoice Basically it rolls a dice with Math.random, multiplies that by 3, rounds that to an integer then picks either 0 1 or 2 on the rock paper scissors "choices" and returns whatever is in that position 0 rock 1 paper 2 scissors

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello World!")
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * 3); // generate number between 0 and 2
+    return choices[randomNumber]; //  assigns the number and returns the pick
+  }
